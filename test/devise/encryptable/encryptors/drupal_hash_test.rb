92f495911643c06d436f1a60b90b8e28e16d999d
Jeroen Bobbeldijk <jeroen@vdmi.nl>
2014-02-06 12:28:27 +0100

    Add another test for existing hash

--- test/devise/encryptable/encryptors/drupal_hash_test.rb
+++ test/devise/encryptable/encryptors/drupal_hash_test.rb
@@ -3,9 +3,16 @@ require "test_helper"
 class Encryptors < ActiveSupport::TestCase
   include Support::Swappers
 
-  test 'should match a password created by drupal' do
+  test 'should match a password created by devise-drupal' do
     hash = Devise::Encryptable::Encryptors::DrupalHash.digest('admin', nil, nil, nil)
     compare = Devise::Encryptable::Encryptors::DrupalHash.compare(hash, 'admin', nil, nil, nil)
     assert_equal true, compare
   end
+
+  test 'should match a password created by Drupal 7' do
+    hash = "$S$DSECOTv7C.aNnAAPVG.gOY718Xg5U3jFp11u76sqpKGDM3zm.f4O"
+    compare = Devise::Encryptable::Encryptors::DrupalHash.compare(hash, 'admin', nil, nil, nil)
+    assert_equal true, compare
+  end
+
 end
